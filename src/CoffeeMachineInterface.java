9426207db4ce7343f404b4913dde8c0d93e0e28b
Robert Johns <robertjohns08@gmail.com>
2020-10-26 04:16:48 -0400

    final commits

---
+++ src/CoffeeMachineInterface.java
@@ -0,0 +1,8 @@
+
+public interface CoffeeMachineInterface {
+	
+	public  void chooseFirstSelection();
+	
+	
+	public  void chooseSecondSelection();
+}
---
+++ src/CoffeeTouchScreenAdapter.java
@@ -0,0 +1,13 @@
+//Created by Robert Johns
+
+public class CoffeeTouchScreenAdapter {
+	OldCoffeeMachine oldVendingMachine;
+
+	public void chooseFirstSelection() {
+
+	}
+
+	public void chooseSecondSelection() {
+
+	}
+}
